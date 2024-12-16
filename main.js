3786062a39986bb561fcbc33d5aa5c0bdf05ce3a
Akuchinyerenwa OPUS <littlepetero@gmail.com>
2024-12-16 14:49:06 +0100

    add functionality to text-animation on main.js file

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* THIS IS FOR THE MAIN PAGE TIME */
 const hourEL = document.getElementById("hour");
 const minuteEL = document.getElementById("minute");
 const secondEL = document.getElementById("second");
@@ -9,3 +10,19 @@ setInterval(() => {
   minuteEL.textContent = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
   secondEL.textContent = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
 }, 1000);
+/* --------------------------------- */
+
+/* THIS IS FOR THE TEXT ANIMATION */
+const textAnimationEL = document.getElementById("text-animation");
+let textArray = ["Akuchinyerenwa", "A Web Developer", "A Freelancer", "An Instructor", "A Programmer"];
+let arrIndex = 0;
+
+setInterval(() => {
+  textAnimationEL.textContent = `I am ${textArray[arrIndex]}`;
+  arrIndex++;
+
+  if (arrIndex >= textArray.length) {
+    arrIndex = 0;
+  }
+}, 2500);
+/* ------------------------------- */
